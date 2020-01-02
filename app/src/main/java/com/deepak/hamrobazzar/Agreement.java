a3bbede186c1682ff1d28175ad6fa9eeca633c90
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2020-01-02 22:52:21 +0545

    Terms activity added.

--- app/src/main/java/com/deepak/hamrobazzar/Agreement.java
+++ app/src/main/java/com/deepak/hamrobazzar/Agreement.java
@@ -2,13 +2,36 @@ package com.deepak.hamrobazzar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class Agreement extends AppCompatActivity {
+
+    CheckBox chkTerms, chkSafety, chkAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+
+        chkTerms = findViewById(R.id.chkTerms);
+        chkSafety = findViewById(R.id.chkSafety);
+        chkAd = findViewById(R.id.chkAd);
+
+
+        chkTerms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Agreement.this, Terms.class);
+                startActivity(intent);
+                chkTerms.setChecked(true);
+                chkTerms.setEnabled(false);
+
+                //v.setOnClickListener(null);
+            }
+        });
+
     }
 }
