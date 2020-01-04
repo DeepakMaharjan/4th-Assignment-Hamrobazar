bf30e9ee786a6177468132cc240a1e168c7baadf
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2020-01-04 20:10:23 +0545

    Splash Activity created and dashboard activity added.

--- app/src/main/java/com/deepak/hamrobazzar/Agreement.java
+++ app/src/main/java/com/deepak/hamrobazzar/Agreement.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class Agreement extends AppCompatActivity {
 
     CheckBox chkTerms, chkSafety, chkAd;
+    Button btnAgreed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class Agreement extends AppCompatActivity {
         chkTerms = findViewById(R.id.chkTerms);
         chkSafety = findViewById(R.id.chkSafety);
         chkAd = findViewById(R.id.chkAd);
+        btnAgreed = findViewById(R.id.btnAgreed);
 
 
         chkTerms.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +43,24 @@ public class Agreement extends AppCompatActivity {
                 startActivity(intent);
                 chkSafety.setChecked(true);
                 chkSafety.setEnabled(false);
+            }
+        });
+
+        chkAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Agreement.this, AdPostingRule.class);
+                startActivity(intent);
+                chkAd.setChecked(true);
+                chkAd.setEnabled(false);
+            }
+        });
+
+        btnAgreed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Agreement.this, SplashActivity.class);
+                startActivity(intent);
             }
         });
 
